3780f3c2cf94a89cdb61bf00ca037e621786ac80
Farid <faridfaracli9@gmail.com>
2024-11-16 19:39:47 +0400

    add clean command

--- cbuild.c
+++ cbuild.c
@@ -1,9 +1,37 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int
-main(void)
+void
+build()
 {
 	system("cc -c sample.c");
 	system("cc -o sample sample.o");
-	return 0;
+}
+
+void
+clean()
+{
+	system("rm *.o sample");
+}
+
+void
+usage()
+{
+	fprintf(stderr, "usage: ./build [clean]\n");
+}
+
+int
+main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		build();
+		return 0;
+	}
+	if (!strcmp(argv[1], "clean")) {
+		clean();
+		return 0;
+	}
+	usage();
+	return 1;
 }
